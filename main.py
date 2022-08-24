da65a9d5645e384b00acf2afc7574c1a15f8ad24
sd950216 <meen79508@gmail.com>
2022-08-24 04:30:31 +0200

    fixed common errors

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
 
